77b42d4e2fe40aca0bc6d2012b7913671a5030d9
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-11-24 09:36:58 +0600

    Project setup and navbar

---
+++ src/Layouts/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Main = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+};
+
+export default Main;
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#179BA2",
+
+          secondary: "#524dea",
+
+          accent: "#e085fc",
+
+          neutral: "#24233E",
+
+          "base-100": "#FAF8FC",
+
+          info: "#81CCDF",
+
+          success: "#1FDBCB",
+
+          warning: "#DCA204",
+
+          error: "#F03B24",
+        },
+      },
+    ],
+  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+};
