89b55ef8bcbeec0ebbde0bf8108f20194ef980e0
Md Suzayet Hossan <md.suzayet.377@gmail.com>
2022-11-26 12:46:17 +0600

    sellers load and show

--- src/pages/Dashboard/Seller/AllSellers.js
+++ src/pages/Dashboard/Seller/AllSellers.js
@@ -1,7 +1,91 @@
-import React from "react";
-
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../contexts/AuthProvider";
+import toast from "react-hot-toast";
 const AllSellers = () => {
-  return <div>AllSellers</div>;
+  const { user } = useContext(AuthContext);
+  const url = `${process.env.REACT_APP_domain}/sellers`;
+  const { data: sellers = [], refetch } = useQuery({
+    queryKey: ["sellers", user?.email],
+    queryFn: async () => {
+      const res = await fetch(url, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      });
+      const data = await res.json();
+      return data;
+    },
+  });
+  const handleDelete = (id) => {
+    const url = `${process.env.REACT_APP_domain}/user/delete/${id}`;
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        authorization: `bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          toast.success("Product deleted successfully");
+          refetch();
+        }
+      });
+  };
+  return (
+    <div>
+      <h2 className="font-bold">My sellers</h2>
+      <div className="overflow-x-auto mt-7">
+        <table className="table table-zebra w-full">
+          <thead>
+            <tr>
+              <th>SN</th>
+              <th>Name</th>
+              <th>Image</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sellers.length &&
+              sellers?.map((seller, index) => (
+                <tr key={seller._id}>
+                  <th>{index + 1}</th>
+                  <td>
+                    <div className="avatar">
+                      <div className="w-14 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={seller.image} alt="" />
+                      </div>
+                    </div>
+                  </td>
+                  <td>{seller.name}</td>
+                  <td>{seller.email}</td>
+                  <td>
+                    {" "}
+                    <svg
+                      onClick={() => handleDelete(seller._id)}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6 cursor-pointer"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllSellers;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -6,6 +6,7 @@ import { AuthContext } from "../contexts/AuthProvider";
 import useToken from "../hooks/useToken";
 const Register = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const { createUser, updateUser } = useContext(AuthContext);
   const [createdEmail, setCreatedEmail] = useState("");
   const [token] = useToken(createdEmail);
@@ -14,6 +15,7 @@ const Register = () => {
   }
   const handleRegister = (event) => {
     event.preventDefault();
+    setLoading(true);
     const form = event.target;
     const user = {
       name: form.name.value,
@@ -31,7 +33,9 @@ const Register = () => {
         .then((result) => {
           saveUser(user.image, user);
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          setLoading(false);
+        });
     });
   };
   const saveUser = (image, userInfo) => {
@@ -52,9 +56,15 @@ const Register = () => {
           .then((result) => {
             console.log(result);
             setCreatedEmail(email);
+            console.log(loading);
+          })
+          .catch(() => {
+            setLoading(false);
           });
       })
-      .catch((err) => console.log(err.message));
+      .catch((err) => {
+        setLoading(false);
+      });
   };
   return (
     <div className="hero min-h-screen bg-base-200">
@@ -87,6 +97,7 @@ const Register = () => {
               </label>
               <input
                 type="file"
+                required
                 name="image"
                 className="file-input file-input-bordered w-full max-w-xs"
               />
@@ -97,6 +108,7 @@ const Register = () => {
               </label>
               <select
                 name="role"
+                required
                 className="select focus:border-none select-bordered w-full max-w-xs"
               >
                 <option value="buyer">Buyer</option>
@@ -128,7 +140,28 @@ const Register = () => {
               />
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Register</button>
+              {(loading && (
+                <div
+                  aria-label="Loading..."
+                  role="status"
+                  className="text-center w-full"
+                >
+                  <svg class="h-6 w-6 animate-spin" viewBox="3 3 18 18">
+                    <path
+                      class="fill-gray-200"
+                      d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
+                    ></path>
+                    <path
+                      class="fill-gray-800"
+                      d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"
+                    ></path>
+                  </svg>
+                </div>
+              )) || (
+                <button type="submit" className="btn btn-primary">
+                  Register
+                </button>
+              )}
 
               <label className="label">
                 <span className="label-text-alt">
